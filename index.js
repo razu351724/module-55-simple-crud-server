093a2ec7641df6c41f912631053943fa6af66e12
RAZU-sheikh <razu351724@gmail.com>
2023-10-15 08:12:49 +0600

    delete server side user id stems

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 const app =  express();
 const port = process.env.PORT || 5000;
 
@@ -31,8 +31,16 @@ async function run() {
 
     // এটা আমরা node mongodb crud থেকে নিছি 12, 13 লাইন
     // usage examples> Insert Operations> Insert a Document
-    const database = client.db("usersDB");
-    const userCollection = database.collection("users");
+    // const database = client.db("usersDB");
+    // const userCollection = database.collection("users");
+    //userCollection একবারেও করা য়ায়।
+    const userCollection = client.db('userDB').collection('users');
+
+    app.get('/users', async(req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
     app.post('/users', async(req,res) => {
       const user = req.body;
@@ -41,6 +49,14 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id
+      console.log('Please dalete from database', id);
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
